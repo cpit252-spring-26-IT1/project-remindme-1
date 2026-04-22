cbc9321282339816d7cd6db8ff46491f9a3d2ead
mohammed alharbi <mohd.amh55@gmail.com>
2026-04-22 06:34:17 +0300

    added DocumentRepository, and DocumentService layer #6

---
+++ src/main/java/kau/RemindMe/service/DocumentService.java
@@ -0,0 +1,12 @@
+package kau.RemindMe.service;
+
+import kau.RemindMe.model.Document;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface DocumentService {
+    Document addDocument(String name, String type, LocalDate expairy,String email,String owner,String category);
+    List<Document> getAllDocuments();
+    long daysUntilExpiry(Document doc);
+}
---
+++ src/main/java/kau/RemindMe/service/DocumentServiceImpl.java
@@ -0,0 +1,36 @@
+package kau.RemindMe.service;
+
+import kau.RemindMe.factroy.DocumentFactory;
+import kau.RemindMe.model.Document;
+import kau.RemindMe.repository.DocumentRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+@Service
+public class DocumentServiceImpl implements DocumentService {
+
+    private final DocumentRepository repo;
+
+    public DocumentServiceImpl(DocumentRepository repo) {
+        this.repo = repo;
+    }
+
+
+    @Override
+    public Document addDocument(String name, String type, LocalDate expairy, String email, String owner, String category) {
+        Document doc = DocumentFactory.createDocument(name, type, expairy, email, owner, category);
+        return repo.save(doc);
+    }
+
+    @Override
+    public List<Document> getAllDocuments() {
+        return repo.findAll();
+    }
+
+    @Override
+    public long daysUntilExpiry(Document doc) {
+        return ChronoUnit.DAYS.between(LocalDate.now(), doc.getExpiryDate());
+    }
+}
