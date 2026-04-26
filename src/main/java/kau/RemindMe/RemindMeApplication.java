8f8f6ee6e710eac2bf0b60be5fd5d58b185ced5a
Aziz-Melebari <zezo200811@gmail.com>
2026-04-26 04:57:52 +0300

    created OCRManager for both arabic and english

--- src/main/java/kau/RemindMe/RemindMeApplication.java
+++ src/main/java/kau/RemindMe/RemindMeApplication.java
@@ -9,6 +9,17 @@ public class RemindMeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RemindMeApplication.class, args);
 
+        String path = "C:\\Program Files\\Tesseract-OCR\\tessdata";
+        OCRManager ocr = new OCRManager(path);
+
+
+        String myImage = "src/main/resources/images/sample.png";
+
+        // OCR for Arabic and English combined
+        String result = ocr.extractText(myImage, "ara+eng");
+
+        System.out.println("--- OCR Results ---");
+        System.out.println(result);
 	}
 
 }
