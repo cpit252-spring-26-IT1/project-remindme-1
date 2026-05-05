afeff10150ac4f7a988c1b997d3a3e3de477abd6
Aziz-Melebari <zezo200811@gmail.com>
2026-05-05 03:43:49 +0300

    added support for Mac operating system 18#

--- src/main/java/kau/RemindMe/RemindMeApplication.java
+++ src/main/java/kau/RemindMe/RemindMeApplication.java
@@ -12,8 +12,17 @@ public class RemindMeApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(RemindMeApplication.class, args);
-
-        String path = "C:\\Program Files\\Tesseract-OCR\\tessdata";
+        // Mac support,with the already built windows.
+        String os = System.getProperty("os.name").toLowerCase(); //gets operating system windows or mac.
+        String path;
+        if (os.contains("win")) {   //windows operating system
+            path = "C:\\Program Files\\Tesseract-OCR\\tessdata";
+        } else if (os.contains("mac")) { //mac operating system
+            path = "/opt/homebrew/share/tessdata"; //mac brew
+        } else {
+            path = "/usr/share/tesseract-ocr/5/tessdata"; //mac port
+        }
+        //String path = "C:\\Program Files\\Tesseract-OCR\\tessdata";// old hard coded path (only runs on windows)
         OCRService ocr = new OCRProxy(path);
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
 
